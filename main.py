63ee8f8387ff6324ba82fefd13040a47e85cf91b
Danylo Sulyma <danulo007@gmail.com>
2024-03-29 22:03:30 +0100

    4v

--- main.py
+++ main.py
@@ -1,18 +1,1 @@
-print("Hello World")
-print("Hello Danilo")
-
-
-point = (-4, 0)
-
-match point:
-    case (0, 0):
-        print("Точка в центрі координат")  
-    case (0, y):
-        print(f"Точка лежить на осі Y: y={y}")  
-    case (x, 0):
-        print(f"Точка лежить на осі X: x={x}") 
-    case (x, y):
-        print(f"Точка має координати:  x={x}, y={y}") 
-    case _:
-        print("Це не точка")
- 
+print("hello world")
