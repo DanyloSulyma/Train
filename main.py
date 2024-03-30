1c5eba3cc85b1b4269b42a0d05c6827d45a63c1a
Danylo Sulyma <danulo007@gmail.com>
2024-03-30 18:41:44 +0100

    v5

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("hello world")
+while True:
+    age = input("How old are you? ")
+    try:
+        age = int(age)
+        if age >= 18:
+            print("Accept allowed")
+            break
+        else:
+            print("Accept denied")
+            break
+    except ValueError:
+        print(f"{age} is not number, please write number!")
+    finally:
+        print("-"*14)
