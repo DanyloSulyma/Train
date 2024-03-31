177a50583996cf44adb565f1a351e9ba0066d25c
Danylo Sulyma <danulo007@gmail.com>
2024-03-31 19:08:31 +0200

    v45

--- main.py
+++ main.py
@@ -1,14 +1,24 @@
-while True:
-    age = input("How old are you? ")
-    try:
-        age = int(age)
-        if age >= 18:
-            print("Accept allowed")
-            break
-        else:
-            print("Accept denied")
-            break
-    except ValueError:
-        print(f"{age} is not number, please write number!")
-    finally:
-        print("-"*14)
+# while True:
+#     age = input("How old are you? ")
+#     try:
+#         age = int(age)
+#         if age >= 18:
+#             print("Accept allowed")
+#             break
+#         else:
+#             print("Accept denied")
+#             break
+#     except ValueError:
+#         print(f"{age} is not number, please write number!")
+#     finally:
+#         print("-"*14)
+
+a = "(25+48)**2-(46/2)"
+b = "(45+48)"
+print("c=a + b") 
+print("c =", a, "+", b, "=", eval(a)+ eval(b))
+
+a = 70>>3
+b = ~a
+c = a<<1
+print(a, b, c)
