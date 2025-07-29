f51ab83ee21ecde986a6fe86656385c147890416
DANYLO SULYMA <Danulo007@gmail.com>
2025-07-29 09:47:50 +0200

    ddd

--- main.py
+++ main.py
@@ -1,15 +1,5 @@
-name = input('What is your name? ')
-age = input('How old are you? ')
-city_life = input('Where do you live? ')
+name = "Danylo"
+age = 29
 
-info = f'My name is {name}, im {age} years old, i live in {city_life}'
-
-name_strit = input('What is the name of the street where you live? ')
-
-FLOORS = 9
-APARTAMENT_PER_FLOORS = 4
-apartament_number = int(input('Enter apartament number: '))
-apartament_per_entrance = FLOORS * APARTAMENT_PER_FLOORS
-entrance_number = (apartament_number -1) // apartament_per_entrance + 1
-print(f'{info}, streeet name : {name_strit} {apartament_number}, Entrance number: {entrance_number}')
-
+message = f"my name is {name}. I am {age} years old"
+print(message)
