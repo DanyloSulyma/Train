8dc319d9992571e8a1e172d6469cb44945b4f025
DANYLO SULYMA <Danulo007@gmail.com>
2025-07-29 09:47:56 +0200

    cvcx

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-name = "Danylo"
-age = 29
+my_dict = {"name": "Alice", "age": 25, "city": "New York"}
+print(my_dict["name"])  # Виведе 'Alice'
 
-message = f"my name is {name}. I am {age} years old"
-print(message)
+print(my_dict)
+
+my_dict["age"] = 26  # Змінює вік на 26
+my_dict["email"] = "alice@example.com"  # Додає нову пару ключ-значення
+print(my_dict)
